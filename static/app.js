e19d491f023f7b776f1771b0566a5a614a1e4d66
Kownsora <cielo7824@gmail.com>
2023-07-07 01:01:55 +0900

    메모 수정
    메모 수정

--- static/app.js
+++ static/app.js
@@ -1,17 +1,56 @@
+async function editMemo(event) {
+  const id = event.target.dataset.id;
+  const editInput = prompt("수정할 값을 입력하세요.");
+  const res = await fetch(`/memos/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      id: id,
+      content: editInput,
+    }),
+  });
+  readMemo();
+}
+
+function displayMemos(memo) {
+  const ul = document.querySelector("#memo-ul");
+  const li = document.createElement("li");
+
+  li.innerText = `[id:${memo.id}] ${memo.content}`;
+
+  const editBtn = document.createElement("button");
+  editBtn.innerText = "수정하기";
+  editBtn.addEventListener("click", editMemo);
+  editBtn.dataset.id = memo.id;
+
+  ul.appendChild(li);
+  ul.appendChild(editBtn);
+}
+
+async function readMemo() {
+  const res = await fetch("/memos"); //GET방식
+  const jsonRes = await res.json(); //jsonRes = [{id:123,content:'블라블라'}]
+  const ul = document.querySelector("#memo-ul");
+  ul.innerHTML = "";
+  jsonRes.forEach(displayMemos); //결과값을 출력
+  console.log(jsonRes);
+}
+
 async function createMemo(value) {
-  console.log("입력값은 :", value);
   const res = await fetch("/memos", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      id: new Date(),
+      id: new Date().getTime(),
       content: value,
     }),
   });
   const jsonRes = await res.json();
-  console.log(jsonRes); //결과값
+  readMemo();
 }
 
 function handleSubmit(event) {
@@ -23,3 +62,5 @@ function handleSubmit(event) {
 
 const form = document.querySelector("#memo-form");
 form.addEventListener("submit", handleSubmit);
+
+//readMemo();
